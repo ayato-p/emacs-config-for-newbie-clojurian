7271161ebc51b22c31a284d20942d259f8326975
ayato-p <ayato.skycrawlers@gmail.com>
2017-06-30 16:22:04 +0900

    Needs exec-path-from-shell when windows-system is x

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
 
 ;;; set up exec-path
 (use-package exec-path-from-shell
-  :if (memq window-system '(mac ns))
+  :if (memq window-system '(mac ns x))
   :config
   (exec-path-from-shell-initialize))
 
